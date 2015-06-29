b145428e0c2f2e80138d7de517e5cc50672212c6
ptrnov <ptr.nov@gmail.com>
2015-06-29 11:56:32 +0700

    HRD 29-06-15
    view

--- lukisongroup/views/hrd/employe/view.php
+++ lukisongroup/views/hrd/employe/view.php
@@ -2,7 +2,7 @@
 
 
 use yii\helpers\Html;
-use yii\widgets\DetailView;
+use kartik\detail\DetailView;
 use yii\bootstrap\Modal;
 use kartik\widgets\ActiveField;
 use kartik\widgets\ActiveForm;
@@ -13,60 +13,57 @@ use kartik\widgets\FileInput;
 /* @var $this yii\web\View */
 /* @var $model app\models\maxi\Maxiprodak */
 
-$this->title = $model->EMP_ID;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Maxiprodaks'), 'url' => ['prodak']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Maxiprodaks'), 'url' => ['prodak']];
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="maxiprodak-view">
-<?php
-	
-	$form = ActiveForm::begin();
-    $form2;
-	//$form2;
-    ActiveForm::end();
-	
-	$form1= FileInput::widget([
-		//'name' => 'attachment_49[]',
-		//'options'=>[
-		//	'multiple'=>true
-		//],
-		'model' => $model,
-        'attribute' => 'EMP_ID',
-		'pluginOptions' => [
-			'allowedFileExtensions'=>['jpg', 'gif', 'png', 'bmp'],
-			 'showUpload' => true,	
-			'initialPreview'=>[
-				Html::img("http://192.168.56.101/advanced/lukisongroup/web/css/image/" .$model->EMP_IMG, ['class'=>'file-preview-image', 'alt'=>'The Moon', 'title'=>'The Moon']),
-				//Html::img("http://placeimg.com/200/150/people/2.jpg",  ['class'=>'file-preview-image', 'alt'=>'The Earth', 'title'=>'The Earth']),
-				//Html::img("http://placeimg.com/200/150/people/2.jpg",  ['class'=>'file-preview-image', 'alt'=>'The Earth', 'title'=>'The Earth']),
-			],
-			'initialPreviewConfig' => [
-				'{caption: "People-1.jpg", width: "20px", url: "/site/file-delete", key: 1}',
-				'{caption: "People-2.jpg", width: "20px", url: "/site/file-delete", key: 2}'
-			],
-			//'initialCaption'=>"The Moon and the Earth",
-			'overwriteInitial'=>false
-		]
-	]);
-	//$form = ActiveForm::begin(['type'=>ActiveForm::TYPE_INLINE]);
-	$form2= Growl::widget([
-	'type' => Growl::TYPE_MINIMALIST,
-	'title' => 'Kartik Visweswaran',
-	'icon' => 'http://placeimg.com/200/150/people/2',
-	'iconOptions' => ['class'=>'img-circle pull-left'],
-	'body' => 'Momentum reduce child mortality effectiveness incubation empowerment connect.',
-	'showSeparator' => false,
-	'delay' => 700,
-	'pluginOptions' => [
-		'icon_type'=>'image',
-		'showProgressbar' => false,
-		'placement' => [
-			'from' => 'top',
-			'align' => 'right',
-		],
-	]
-	]);
-	//ActiveForm::end();
-	
-	?>
+
+<div class="panel panel-default" style="margin-top: 0px">
+     <div class="panel-body">
+		<?php
+			echo DetailView::widget([
+				'model' => $model,
+				
+				'condensed'=>true,
+				'hover'=>true,
+				'mode'=>DetailView::MODE_VIEW,
+				'panel'=>[
+					'heading'=>$model->EMP_NM . ' '.$model->EMP_NM_BLK,
+					'type'=>DetailView::TYPE_INFO,
+				],	
+				
+				
+				
+				'attributes' => [
+					'EMP_ID',		
+					'EMP_NM',					
+					'EMP_NM_BLK',
+					'EMP_IMG',
+
+					// Employe Coorporation - Author: -ptr.nov-
+					'EMP_CORP_ID' ,
+					'DEP_ID',
+					'EMP_GENDER',
+					'EMP_STS',
+					'JAB_ID' ,
+					'EMP_IMG' ,
+					//Employe Profile - Author: -ptr.nov-
+					'EMP_KTP' ,
+					'EMP_ALAMAT' ,
+					'EMP_ZIP' ,
+					'EMP_TLP' ,
+					'EMP_HP' ,
+					'EMP_EMAIL' ,
+					'GRP_NM',
+					'EMP_JOIN_DATE',
+					//Join
+					//'corpOne.CORP_NM' ,
+					//'deptOne.DEP_NM' ,
+					//'jabOne.JAB_NM' ,
+					//'sttOne.STS_NM',	
+				],
+				
+			]);			
+		?>
+	</div>
 </div>
+
